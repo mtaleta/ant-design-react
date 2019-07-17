af9a10e19bb850efdb941f138c4ee5ce50d94cac
mtal <ex051240@gmail.com>
2019-07-17 23:35:06 +0800

    add radio狀態改變

--- src/App.js
+++ src/App.js
@@ -4,46 +4,18 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import reqwest from 'reqwest';
 
-const columns = [
-  {
-    title: '賽事',
-    dataIndex: 'league[0]',
-  },
-  {
-    title: '時間',
-    dataIndex: 'matchTime',
-    render: (value, record) => <span title={record.matchYear + "-" + record.matchDate + " " + record.matchTime}> {record.matchDate + " " + record.matchTime}</span>,
-  },
-  {
-    title: '主隊',
-    dataIndex: 'home[0]',
-  },
-  {
-    title: '全場比分',
-    dataIndex: 'score',
-    render: (value, record) => <span>{record.homeScore} - {record.guestScore}</span>,
-  },
-  {
-    title: '客隊',
-    dataIndex: 'guest[0]',
-  },
-  {
-    title: '半場比分',
-    dataIndex: 'halfScore',
-    render: (value, record) => <span>{record.homeHalfScore} - {record.guestHalfScore}</span>,
 
-  },
-];
 
 class App extends Component {
   //保存資料
   state = {
     data: [],
     loading: false,
+    league: 0,
   };
 
   fetch = () => {
-    this.setState({loading:true})
+    this.setState({ loading: true })
     reqwest({
       url: '/result.json',
       method: 'get',
@@ -60,8 +32,47 @@ class App extends Component {
     this.fetch();
   }
 
-
+  handleLangChange = (e) => {
+    this.setState({
+      league: e.target.value
+    })
+  }
   render() {
+    const columns = [
+      {
+        title: '賽事',
+        dataIndex: 'league',
+        render: league => <span>{league[this.state.league]}</span>
+      },
+      {
+        title: '時間',
+        dataIndex: 'matchTime',
+        render: (value, record) => <span title={record.matchYear + "-" + record.matchDate + " " + record.matchTime}> {record.matchDate + " " + record.matchTime}</span>,
+      },
+      {
+        title: '主隊',
+        dataIndex: 'home',
+        render: home => <span>{home[this.state.league]}</span>
+
+      },
+      {
+        title: '全場比分',
+        dataIndex: 'score',
+        render: (value, record) => <span>{record.homeScore} - {record.guestScore}</span>,
+      },
+      {
+        title: '客隊',
+        dataIndex: 'guest',
+        render: guest => <span>{guest[this.state.league]}</span>
+
+      },
+      {
+        title: '半場比分',
+        dataIndex: 'halfScore',
+        render: (value, record) => <span>{record.homeHalfScore} - {record.guestHalfScore}</span>,
+
+      },
+    ];
     return (
       <div className="App">
         <nav className="navbar navbar-light bg-light">
@@ -75,10 +86,10 @@ class App extends Component {
             <Checkbox>顯示紅牌</Checkbox>
             <Checkbox>顯示黃牌</Checkbox>
 
-            <Radio.Group defaultValue="{0}">
-              <Radio.Button value="{0}">簡體</Radio.Button>
-              <Radio.Button value="{1}">繁體</Radio.Button>
-              <Radio.Button value="{2}">English</Radio.Button>
+            <Radio.Group defaultValue={this.state.league} onChange={this.handleLangChange}>
+              <Radio.Button value={0}>簡體</Radio.Button>
+              <Radio.Button value={1}>繁體</Radio.Button>
+              <Radio.Button value={2}>English</Radio.Button>
             </Radio.Group>
           </div>
           <Table
