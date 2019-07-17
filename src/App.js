a7905c640ed60dab0ef8ac5df42f7e049c4b1079
mtal <ex051240@gmail.com>
2019-07-17 22:04:35 +0800

    add table

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import React, { Component } from 'react';
-import { Checkbox, Radio } from 'antd';
+import { Checkbox, Radio, Table } from 'antd';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+const dataSource = [
+  {
+    key: '1',
+    name: '胡彦斌',
+    age: 32,
+    address: '西湖区湖底公园1号',
+  },
+  {
+    key: '2',
+    name: '胡彦祖',
+    age: 42,
+    address: '西湖区湖底公园1号',
+  },
+];
+
+const columns = [
+  {
+    title: '姓名',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: '年龄',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: '住址',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
 
 class App extends Component {
   render() {
@@ -14,13 +48,17 @@ class App extends Component {
         </nav>
 
         <div className="container mt-3">
-          <Checkbox>顯示紅牌</Checkbox>
-          <Checkbox>顯示黃牌</Checkbox>
-          <Radio.Group defaultValue="{0}">
-            <Radio.Button value="{0}">簡體</Radio.Button>
-            <Radio.Button value="{1}">繁體</Radio.Button>
-            <Radio.Button value="{2}">English</Radio.Button>
-          </Radio.Group>
+          <div className="filter my-3">
+            <Checkbox>顯示紅牌</Checkbox>
+            <Checkbox>顯示黃牌</Checkbox>
+
+            <Radio.Group defaultValue="{0}">
+              <Radio.Button value="{0}">簡體</Radio.Button>
+              <Radio.Button value="{1}">繁體</Radio.Button>
+              <Radio.Button value="{2}">English</Radio.Button>
+            </Radio.Group>
+          </div>
+          <Table dataSource={dataSource} columns={columns} size="middle" pagination={false} />
         </div>
       </div>
     );
